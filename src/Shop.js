1ab087e416cf651934ae06e31824329335092b6f
Kaneez Fatima <kf18800@gmail.com>
2024-08-25 05:21:00 -0700

    ReduxStore

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h3>Buy Adidas shoes 50 Rs</h3>
+      <button className='btn btn-primary '>-</button>
+      Add to cart
+      <button className='btn btn-primary '>+</button>
+    </div>
+  )
+}
+
+export default Shop
